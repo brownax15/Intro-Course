80d707d8675c64ae58e5a4c6eb4aeb1d71c2ec88
Allison <Allison@Allison_Vaio.setonhill.edu>
2019-09-05 12:15:44 -0400

    Project for review

---
+++ Poem/src/Dickinson.java
@@ -0,0 +1,28 @@
+
+public class Dickinson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Poem Name: Success is counted sweetest
+		//Author: Emily Dickinson
+		
+		
+		System.out.println("");
+		System.out.println("Success is counted sweetest");
+		System.out.println("By those who ne'er succeed.");
+		System.out.println("To comprehend a nectar");
+		System.out.println("Requires sorest need.");
+		System.out.println("");
+		System.out.println("Not one of all the purple Host");
+		System.out.println("Who took the Flag today");
+		System.out.println("Can tell the definition");
+		System.out.println("So clear of victory");
+		System.out.println("");
+		System.out.println("As he defeated – dying –");
+		System.out.println("On whose forbidden ear");
+		System.out.println("The distant strains of triumph");
+		System.out.println("Burst agonized and clear!");
+	}
+
+}
