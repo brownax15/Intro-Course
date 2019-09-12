a3c3a7ff390ee286bbcb0d9372ec6fce49451b53
Allison <Allison@Allison_Vaio.home>
2019-09-12 18:40:17 -0400

    final version

---
+++ Brown Program 1/src/Program1.java
@@ -0,0 +1,26 @@
+//Project Name: Program 1
+//Author: Allison Brown
+//Description: Using variables and comments
+//Last Modified: 10.9.19
+//Version: 1.0
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//setting variables
+		int age = 26;
+		String myName = "Allison";
+		short myNumber = 3;
+		
+		//output(introduction) using variables
+		System.out.println("Here is some information about me." + "\n");
+		System.out.print("My name: " + myName + "\nMy age: " + age);
+		System.out.println("\nMy favorite number: " + myNumber);
+		
+		
+		
+	}
+
+}
